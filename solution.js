f883afbb155f43dc628439a9b7d6b509252e1a9d
Sathish-Veerasamy <sathish2181997@gmail.com>
2022-12-21 08:12:31 +0530

    i have done the solution part with correct code

--- solution.js
+++ solution.js
@@ -1,6 +1,24 @@
 // complete the given function
 
 function palindrome(str){
+ 
+   var i,j;
+   i=0;
+   j=str.length-1;
+//meat //refer
+   while(i<j){
+    if(str[i].toLowerCase()===str[j].toLowerCase()){
+        i++;
+        j--;
+        if(i===j){
+            return true;
+        }
+    }else{
+        return false;
+    }
+   }
+
 
 }
+
 module.exports = palindrome
